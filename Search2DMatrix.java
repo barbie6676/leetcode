26c37890d7e3944cb53a9a854c33fe0686f3cee0
barbie6676 <mian@lexity.com>
2013-10-10 00:09:14 -0700

    Create Search2DMatrix.java

---
+++ Search2DMatrix.java
@@ -0,0 +1,28 @@
+/*74. search a 2D matrix
+algorithm: same as the binary search, O(log(m*n)) or O(log(m)+log(n))...
+*/
+
+
+public boolean searchMatrix(int[][] matrix, int target) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int m = matrix.length;
+        if ( m ==0) return false;
+        int n = matrix[0].length;
+        if (n ==0) return false;
+        
+        int l = 0; 
+        int r = m*n-1;
+        while (l<=r) {
+            int mid = (l+r)/2;
+            
+            if (matrix[mid/n][mid%n] == target) return true;
+            
+            if (matrix[mid/n][mid%n] < target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        
+        return false;
+    }
