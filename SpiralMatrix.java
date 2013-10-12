3d3373010b8cb26ef4a2e77203ef65f4baca6e4e
barbie6676 <mian@lexity.com>
2013-10-11 22:12:57 -0700

    Update SpiralMatrix.java

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -1,49 +1,99 @@
-/*59. spiral matrix II
+/*
+54. spiral matrix 
+
+four boundaries, four directions.
+
+59. spiral matrix II
 algorithm:  marker at top line, bottom line, left col and right col.
 iterate four directions accordingly
 
 */
-
-public int[][] generateMatrix(int n) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        int[][] matrix = new int[n][n];
+public ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        
+        int m = matrix.length;
+        if (m == 0) return ret;
+        int n = matrix[0].length;
+        if (n == 0) return ret;
         
         int top = 0;
-        int right = n;
-        int bottom = n;
+        int right = n-1;
+        int bottom = m-1;
         int left = 0;
         
-        int it = 0;
-        int num = 1;
+        int it = left;
         int direction = 0;
         
-        while(right > left && bottom > top) {
-            if (direction == 0) { //left -> right
-                for (it = left; it < right; it++){
-                    matrix[top][it] = num++;
+        while (left <= right && top <= bottom) {
+            if (direction == 0) {
+                for (it = left; it <= right ; it++){
+                    ret.add(matrix[top][it]);
+                }
+                top++;
+                direction++;
+            } else if (direction == 1) {
+                for (it = top;it<=bottom;it++){
+                    ret.add(matrix[it][right]);
                 }
                 right--;
-                direction ++;
-            } else if(direction == 1) {//top -> down
-                for (it = top+1;it<bottom;it++){
-                    matrix[it][right] = num++;
+                direction++;
+            } else if (direction == 2) {
+                for (it = right ; it>=left;it--){
+                    ret.add(matrix[bottom][it]);
                 }
                 bottom--;
-                direction ++;
-                
-            } else if (direction == 2) {//right -> left
-                for(it = right-1; it >= left; it--){
-                    matrix[bottom][it] = num++;
-                }
                 direction++;
-                
-            } else { // bottom -> up
-                for (it = bottom -1; it > top;it--){
-                    matrix[it][left] = num++;
+            } else {
+                for (it = bottom ; it >= top;it--){
+                    ret.add(matrix[it][left]);
                 }
                 direction = 0;
                 left++;
+            }
+        }
+        
+        return ret;
+}
+
+
+public int[][] generateMatrix(int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+       int[][] matrix = new int[n][n];
+        int num = 1;
+        
+        int top = 0;
+        int right = n-1;
+        int bottom = n-1;
+        int left = 0;
+        
+        int it = left;
+        int direction = 0;
+        
+        while (left <= right && top <= bottom) {
+            if (direction == 0) {
+                for (it = left; it <= right ; it++){
+                    matrix[top][it] = num++;
+                }
                 top++;
+                direction++;
+            } else if (direction == 1) {
+                for (it = top;it<=bottom;it++){
+                    matrix[it][right]= num++;
+                }
+                right--;
+                direction++;
+            } else if (direction == 2) {
+                for (it = right ; it>=left;it--){
+                    matrix[bottom][it]= num++;
+                }
+                bottom--;
+                direction++;
+            } else {
+                for (it = bottom ; it >= top;it--){
+                    matrix[it][left]= num++;
+                }
+                direction = 0;
+                left++;
             }
         }
         
