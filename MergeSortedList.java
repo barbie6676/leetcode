f2dd362a24788741f28d586d6e9f21321b08ff81
barbie6676 <mian@lexity.com>
2013-10-12 16:42:52 -0700

    Update MergeSortedList.java

--- MergeSortedList.java
+++ MergeSortedList.java
@@ -1,4 +1,32 @@
-/*21. merge two sorted lists*/
+/*21. merge two sorted lists
+ 23. merge k sorted lists*/
+ 
+  public ListNode mergeKLists(ArrayList<ListNode> lists) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int n = lists.size();
+        if (n == 0) return null;
+        int i = 0;
+        while(i < lists.size()) {
+            if (lists.get(i)==null) {
+                lists.remove(i);
+            } else {
+                i++;
+            }
+        }
+        
+        n = lists.size();
+        if (n == 0) return null;
+        if ( n== 1) return lists.get(0);
+        
+        while(lists.size()>1){
+            ListNode l = mergeTwoLists(lists.get(0),lists.get(1));
+            lists.set(0,l);
+            lists.remove(1);
+        }
+        
+        return lists.get(0);
+    }
+    
 
  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
