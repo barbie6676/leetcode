f3bd5af716a5d70261201cfb6bc544c49d654a6d
barbie6676 <mian@lexity.com>
2013-10-13 17:38:41 -0700

    Update SingerNum.java

--- SingerNum.java
+++ SingerNum.java
@@ -9,3 +9,24 @@ public int singleNumber(int[] A) {
         }
         return ret^0;
     }
+    
+    public int singleNumber(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int n = A.length;
+        if(n == 0){
+            return -1;
+        }
+         
+         int[] counts = new int[32];
+         int result = 0;
+         for(int i = 0; i < 32; i++){
+            for(int j = 0; j < n; j++){
+                 if(((A[j] >> i) & 1) == 1){
+                     counts[i] = (counts[i] + 1) % 3;
+                 }
+             }
+             result |= (counts[i] << i);
+         }
+         
+        return result;
+    }
