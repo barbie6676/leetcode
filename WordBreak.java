ff2c4ee4d232ea1ee1fa8e3a51c042c8225f6b48
barbie6676 <mian@lexity.com>
2013-10-19 22:15:34 -0700

    Update WordBreak.java

--- WordBreak.java
+++ WordBreak.java
@@ -1,3 +1,23 @@
+//correct DP solution
+// http://www.mitbbs.com/article_t/JobHunting/32547435.html 
+//level 4
+
+public boolean wordBreak(String s, Set<String> dict) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+        for (int i = 1; i <= s.length(); i++){
+            for (int j = 0; j < i; j++){
+                if (f[j] && dict.contains(s.substring(j, i))) {
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+        return f[s.length()];
+    }
+    
+
 /*pass the test, but the code is incorrect.
 
 it will false the case "abcde" {"ab","cde","abc"}
