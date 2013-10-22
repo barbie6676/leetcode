6a534c83bc71eda1a865a1def26a35657d2321a7
barbie6676 <mian@lexity.com>
2013-10-22 00:21:33 -0700

    Update Search2DMatrix.java

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -2,7 +2,24 @@
 algorithm: same as the binary search, O(log(m*n)) or O(log(m)+log(n))...
 */
 
-
+// here is the correct version of O(m+n) solution
+public boolean searchMatrix(int[][] matrix, int target) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+        if (n == 0) return false;
+        int i = 0;
+        int j = n-1;
+        while (i<m&&j>=0){
+            if (matrix[i][j]>target) j--;
+            else if (matrix[i][j]<target)i++;
+            else return true;
+        }
+        return false;
+    }
+    
+    
 public boolean searchMatrix(int[][] matrix, int target) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         int m = matrix.length;
